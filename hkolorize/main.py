ee0d2c3e3452bd794dd257e392b17aa6fa87598d
Thomas Buida <thomas@gamewisp.com>
2018-04-29 11:44:08 -0500

    add image processing to get dominate color using kmeans clustering

--- hkolorize/main.py
+++ hkolorize/main.py
@@ -1,12 +1,23 @@
 from hue import Hue
+from PIL import Image
+
 import settings
+import process_img
 
 def main():
     light_names = settings.LIGHT_NAMES
     bridge_ip = settings.BRIDGE_IP
 
+    # img = Image.open('/Users/gamewisp/Desktop/hue_test_images/trees.jpg')
+    img = Image.open('/Users/gamewisp/Desktop/hue_test_images/beach.jpg')
+    cluster_centers, dominant_color = process_img.get_colors(img, 3)
+    print(cluster_centers)
+    print(dominant_color)
+
+
     hue = Hue(bridge_ip, light_names)
-    hue.set_lights_random()
+    # hue.set_lights_random()
+    hue.set_lights_rgb(dominant_color)
 
 if __name__ == '__main__':
     main()
